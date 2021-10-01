d8091c8b0889bef6980384d541c165355f949f88
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2021-10-01 15:51:21 +0300

    Remove MSBuild reference

--- src/fsharp/SimulatedMSBuildReferenceResolver.fs
+++ src/fsharp/SimulatedMSBuildReferenceResolver.fs
@@ -5,12 +5,15 @@ module internal FSharp.Compiler.CodeAnalysis.SimulatedMSBuildReferenceResolver
 open System
 open System.IO
 open System.Reflection
-open Microsoft.Build.Utilities
 open Internal.Utilities.Library
 open FSharp.Compiler.IO
 
 #if !FX_NO_WIN_REGISTRY
 open Microsoft.Win32
+#endif
+
+#if !NETSTANDARD
+open Microsoft.Build.Utilities
 #endif
 
 // ATTENTION!: the following code needs to be updated every time we are switching to the new MSBuild version because new .NET framework version was released
@@ -52,6 +55,7 @@ let SupportedDesktopFrameworkVersions = [ Net48; Net472; Net471; Net47; Net462; 
 
 let private SimulatedMSBuildResolver =
 
+#if !NETSTANDARD
     /// Get the path to the .NET Framework implementation assemblies by using ToolLocationHelper.GetPathToDotNetFramework
     /// This is only used to specify the "last resort" path for assembly resolution.
     let GetPathToDotNetFrameworkImlpementationAssemblies v =
@@ -74,6 +78,7 @@ let private SimulatedMSBuildResolver =
             | null -> []
             | x -> [x]
         | _ -> []
+#endif
 
     let GetPathToDotNetFrameworkReferenceAssemblies version =
 #if NETSTANDARD
@@ -148,7 +153,9 @@ let private SimulatedMSBuildResolver =
                     yield! registrySearchPaths()
 #endif
                 yield! GetPathToDotNetFrameworkReferenceAssemblies targetFrameworkVersion
+#if !NETSTANDARD
                 yield! GetPathToDotNetFrameworkImlpementationAssemblies targetFrameworkVersion
+#endif
               ]
 
             for r, baggage in references do
