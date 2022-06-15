d54b5c000569032125e80d202df80ed33db9c47c
janusz <janusz.wrobel.2@gmail.com>
2022-06-15 22:34:20 +0100

    generate root+leaves solution, start parallelising queries

--- FCSTest/UnitTest1.fs
+++ FCSTest/UnitTest1.fs
@@ -5,6 +5,8 @@ open System.Diagnostics
 open System.IO
 open FSharp.Compiler.CodeAnalysis
 open FSharp.Compiler.Text
+open Microsoft.Build.Construction
+open Microsoft.Build.Evaluation
 open NUnit.Framework
 open Newtonsoft.Json
 open Serializing
@@ -12,6 +14,71 @@ open Serializing
 
 [<TestFixture>]
 module X =
+    
+    [<Test>]
+    let Generate () =
+        let dir = "d:/projekty/fsharp/FCSTest/solution"
+        let makePath suffix = Path.Combine(dir, suffix)
+        let sourceFilePath = makePath "library.fs"
+        
+        let makeEmptyProject () =
+            let p = Project(NewProjectFileOptions.None)
+            p.Xml.Sdk <- "Microsoft.NET.Sdk"
+            p.SetProperty("TargetFramework", "net6.0") |> ignore
+            p.SetProperty("DisableImplicitFSharpCoreReference", "true") |> ignore
+            p
+        
+        let writeProject (name : string) =
+            if Directory.Exists (makePath name) then Directory.Delete (makePath name, true)
+            Directory.CreateDirectory (makePath name) |> ignore
+            let dst = makePath (Path.Combine(name, "library.fs"))
+            File.Copy(sourceFilePath, dst)
+            File.WriteAllText(dst, File.ReadAllText(dst).Replace("module FCSTest.library", $"module {name}"))
+            let p = makeEmptyProject()
+            p.AddItem("Compile", "library.fs") |> ignore
+            p.Save(makePath (Path.Combine(name, $"{name}.fsproj")))
+        
+        let name = "root"
+        Directory.Delete(makePath name, true)
+        let slnName = "solution"
+        let slnPath = makePath $"{slnName}.sln"
+        if File.Exists slnPath then File.Delete slnPath
+        Directory.CreateDirectory (makePath name) |> ignore
+        let dst = makePath (Path.Combine(name, "library.fs"))
+        File.Copy(sourceFilePath, dst)
+        let nrs = [1..7]
+        let x =
+            nrs
+            |> List.map (fun i -> $"open leaf_{i}")
+            |> fun l -> String.Join(Environment.NewLine, l)
+        let y =
+            nrs
+            |> List.map (fun i -> $"let x{i} = leaf_{i}.Z1.Y1.X1.sum1 2 3")
+            |> fun l -> String.Join(Environment.NewLine, l)
+        let x = $"{x}{Environment.NewLine}{y}"
+            
+        File.WriteAllText(dst, File.ReadAllText(dst).Replace("module FCSTest.library", $"module FCSTest.library{Environment.NewLine}{x}"))
+        let root = makeEmptyProject()
+        root.AddItem("Compile", "library.fs") |> ignore
+        
+        let runX (args : string) = 
+            let psi = ProcessStartInfo("dotnet", args)
+            psi.WorkingDirectory <- dir
+            let p = Process.Start(psi)
+            p.WaitForExit()
+            printfn $"Running 'dotnet {args}' in '{dir}' returned with exit code {p.ExitCode}"
+        
+        runX "new sln -n solution"
+        
+        for i in nrs do
+            let name = $"leaf_{i}"
+            writeProject name
+            root.AddItem("ProjectReference", $"../{name}/{name}.fsproj") |> ignore
+            runX $"sln add {name}"
+        
+        root.Save(makePath (Path.Combine(name, $"{name}.fsproj")))
+        runX $"sln add {name}"
+    
     let mutable checker : FSharpChecker option = None
 
     [<SetUp>]
@@ -50,6 +117,8 @@ module X =
         let json = Newtonsoft.Json.JsonConvert.SerializeObject(replaced, Formatting.Indented)
         File.WriteAllText(path, json)
         
+    let parallelise (o : )
+        
     let go (name : string) =
         let sw = Stopwatch.StartNew()
         let path = "d:/projekty/fsharp/FCSTest/dumps/" + name
@@ -80,18 +149,28 @@ module X =
     let TwoProjects () =
         go "Program.fs.2022-06-04_003526.json"
         
-    [<Test>]
+        
+    [<TestCase("TwoProjects", "Program.fs.2022-06-04_003526.json")>]
+    [<TestCase("FantomasTopFile", "DaemonTests.fs.2022-06-04_004541.json")>]
+    [<TestCase("FantomasFirstFile", "Parse.fs.2022-06-04_004240.json")>]
+    [<TestCase("root_7leaves", "root_7leaves_library.fs.2022-06-15_222554.json")>]
+    [<TestCase("root_20leaves", "root_20leaves_library.fs.2022-06-15_220456.json")>]
     [<Explicit>]
-    let FantomasEasy () =
-        go "Parse.fs.2022-06-04_004240.json"
-        let calls = Microsoft.FSharp.Collections.CollectionTracing.calls
-        let rows = 
-            calls
-            |> Seq.groupBy (fun e -> e.Function)
-            |> Seq.map (fun (f, entries) -> f, entries |> Seq.toArray |> Array.length)
-            |> Seq.toArray
-        printfn $"+%A{rows}"
-        ()
+    let GoAny (name : string, file : string) =
+        go file
+        
+    // [<Test>]
+    // [<Explicit>]
+    // let FantomasEasy () =
+    //     go "Parse.fs.2022-06-04_004240.json"
+    //     let calls = Microsoft.FSharp.Collections.CollectionTracing.calls
+    //     let rows = 
+    //         calls
+    //         |> Seq.groupBy (fun e -> e.Function)
+    //         |> Seq.map (fun (f, entries) -> f, entries |> Seq.toArray |> Array.length)
+    //         |> Seq.toArray
+    //     printfn $"+%A{rows}"
+    //     ()
 
     
     [<Test>]
