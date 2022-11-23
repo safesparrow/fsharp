2d989051bba63cec65f4ae51589913d0aad5afa2
nojaf <florian.verdonck@outlook.com>
2022-11-23 12:53:20 +0100

    Hard coded test in entry point.

--- tests/ParallelTypeCheckingTests/Program.fs
+++ tests/ParallelTypeCheckingTests/Program.fs
@@ -5,31 +5,40 @@
 open FSharp.Compiler.CompilerConfig
 open ParallelTypeCheckingTests.TestUtils
 
-let _parse (argv: string[]) : Args =
-    let parseMode (mode: string) =
-        match mode.ToLower() with
-        | "sequential" -> Method.Sequential
-        | "parallelfs" -> Method.ParallelCheckingOfBackedImplFiles
-        | "graph" -> Method.Graph
-        | _ -> failwith $"Unrecognised mode: {mode}"
-
-    let path, mode, workingDir =
-        match argv with
-        | [| path |] -> path, Method.Sequential, None
-        | [| path; method |] -> path, parseMode method, None
-        | [| path; method; workingDir |] -> path, parseMode method, Some workingDir
-        | _ -> failwith "Invalid args - use 'args_path [method [fs-parallel]]'"
-
-    {
-        Path = path
-        LineLimit = None
-        Method = mode
-        WorkingDir = workingDir
-    }
+// let _parse (argv: string[]) : Args =
+//     let parseMode (mode: string) =
+//         match mode.ToLower() with
+//         | "sequential" -> Method.Sequential
+//         | "parallelfs" -> Method.ParallelCheckingOfBackedImplFiles
+//         | "graph" -> Method.Graph
+//         | _ -> failwith $"Unrecognised mode: {mode}"
+//
+//     let path, mode, workingDir =
+//         match argv with
+//         | [| path |] -> path, Method.Sequential, None
+//         | [| path; method |] -> path, parseMode method, None
+//         | [| path; method; workingDir |] -> path, parseMode method, Some workingDir
+//         | _ -> failwith "Invalid args - use 'args_path [method [fs-parallel]]'"
+//
+//     {
+//         Path = path
+//         LineLimit = None
+//         Method = mode
+//         WorkingDir = workingDir
+//     }
+open ParallelTypeCheckingTests.Code.TrieApproach
+open ParallelTypeCheckingTests.Code.TrieApproach.DependencyResolution
 
 [<EntryPoint>]
 let main _argv =
-    let args = _parse _argv
-    let args = { args with LineLimit = None }
-    TestCompilationFromCmdlineArgs.TestCompilerFromArgs args
+    let filesWithAST =
+        fcsFiles
+        |> Array.Parallel.mapi (fun idx file ->
+            {
+                Idx = idx
+                AST = FSharp.Compiler.Service.Tests.Common.parseSourceCode (file, System.IO.File.ReadAllText(file))
+                File = file
+            })
+
+    let _graph = mkGraph filesWithAST
     0
